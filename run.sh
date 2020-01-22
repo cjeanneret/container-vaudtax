b032f41562d602d24521a5fecf9db4d05366fd07
Cédric Jeanneret <cjeanneret@internux.ch>
2020-01-22 19:06:30 +0100

    Updated commands, use buildah for building

--- run.sh
+++ run.sh
@@ -2,16 +2,19 @@
 
 year=$(date -d '-1year' '+%Y')
 last_year=$(date -d '-2year' '+%Y')
-podman images vaudtax${yera} | grep -q "localhost/vaudtax${year}" || podman build -t vaudtax${year} $(dirname "$0")
+podman images vaudtax${year} | grep -q "localhost/vaudtax${year}" || \
+  buildah build-using-dockerfile --build-arg year=${year} -t vaudtax${year} $(dirname "$0")
 
 mkdir -p ~/VaudTax${year} ~/VaudTax${last_year}
 podman unshare chown -R 1000:1000 ~/VaudTax${year} ~/VaudTax${last_year}
+xhost local:tax
 
 podman run --rm -ti \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v ~/.Xauthority:/home/tax/.Xauthority:rw \
   -v ~/VaudTax${year}:/home/tax/VaudTax${year}:rw \
   -v ~/VaudTax${last_year}:/home/tax/VaudTax${last_year}:rw \
   -e DISPLAY=unix$DISPLAY \
   --name VaudTax \
-  localhost/vaudtax \
-  ./VaudTax_2019-1.0.2-production-64bit/vaudtax-2019
+  localhost/vaudtax${year} \
+  ./vaudtax-${year}
